54498789f772c479fb54ad3a519843a3feaa032e
MutindaVictoria <victoriaamutinda@gmail.com>
2023-04-11 09:09:07 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,53 @@
+//Write a function that takes in a string and returns it when reversed
+//let food = “eating”
+let food= "eating"
+function results(arr){
+    return arr.reverse('eating')
+    
+}
+console.log (results(arr))
+
+//Given years between 2000 and 2023, 
+//console all the leap years in the following
+//sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap ye
+
+
+
+
+//Write a function that takes in an array of numbers 
+//and returns an array that has all
+//elements multiplied by 4.
+let numArray = [12,87,45,75,23,64,73]
+
+for(items of numArray){
+    return items *4
+}
+console.log(numArray)
+ 
+
+
+//Write a function that takes in the following array and 
+//consoles the target if it is found else
+null
+let num = [2,8,0,23,5,45,76]
+Target = 23
+if (23===(num)){
+    console.log(23)
+}
+else (23!=(num));{
+    console.log(null)
+}
+
+//Write a function that takes in an array of strings and returns an
+// array with every element
+//turned into a number
+//let nums = [“10”,”24”,”45”,”56”,”67”]
+let nums = ["10","24","45","56","67"]
+function numbers(nums){
+    return nums.split().tostring().join();
+
+}
+console.log(numbers(num))
+
+
+
